1063511c9d000b83a4fd7d8b98cec1c2ac164609
William <williammarktaylor@gmail.com>
2018-09-26 22:38:50 +1200

    43215e3w4er53e4r5t345rwe

--- app/src/main/java/com/example/william/gallery/MainActivity.java
+++ app/src/main/java/com/example/william/gallery/MainActivity.java
@@ -1,56 +1,96 @@
 package com.example.william.gallery;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.graphics.Bitmap;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    public class imageAdaptor extends BaseAdapter
+public class MainActivity extends AppCompatActivity
+{
+    //GridView in activity_main.xml
+    GridView gridView;
+
+    //ImageAdapter feeds images into gridView.
+    ImageAdapter imageAdapter;
+
+    //ArrayList containing the URIs for all images on the phone, in date order.
+    private ArrayList<String> listOfURIs = new ArrayList<>();
+
+    //Response for request for storage permission.
+    private static final int REQUEST_TO_READ_EXTERNAL_STORAGE = 1;
+
+    public class ImageAdapter extends BaseAdapter
     {
-        class imageContainer
+        private BitmapFactory.Options pixel = new BitmapFactory.Options();
+
+        public ImageAdapter()
         {
-            int position;
-            ImageView thumbnail;
+            super();
+            pixel.inSampleSize = 6;
         }
 
-        //Required overrides for extension of BaseAdaptor
+        class imageContainer {
+            int position;
+            ImageView thumbnail;
+            String path;
+        }
+
+        //Required overrides for extension of BaseAdapter
         @Override
         public Object getItem(int i) {
             return null;
         }
+
         @Override
         public long getItemId(int i) {
             return i;
         }
+
         @Override
         public int getCount() {
             return 100; //TODO: Fix this;
         }
+
         @Override
-        public View getView(final int i, View convertView, ViewGroup viewGroup) {
+        public View getView(final int i, View convertView, ViewGroup viewGroup)
+        {
+            Log.i("Message", "Enter Get View");
             imageContainer photo;
-            if (convertView == null) {
+            if (convertView == null)
+            {
                 convertView = getLayoutInflater().inflate(R.layout.photo, viewGroup, false);
                 photo = new imageContainer();
                 photo.thumbnail = convertView.findViewById(R.id.photo);
                 convertView.setTag(photo);
-            } else {
-                photo = (imageContainer) convertView.getTag();
+                return convertView;
             }
-            photo.position = i;
-            photo.thumbnail.setImageBitmap(null);
+            else
+            {
+                photo = (imageContainer) convertView.getTag();
+                photo.position = i;
+                photo.thumbnail.setImageBitmap(null);
+                photo.path = listOfURIs.get(i);
+            }
             new AsyncTask<imageContainer, Void, Bitmap>()
             {
-<<<<<<< HEAD
-
-=======
                 private imageContainer photo;
+
                 @Override
                 protected Bitmap doInBackground(imageContainer... params)
                 {
@@ -58,17 +98,75 @@ public class MainActivity extends AppCompatActivity {
                     Bitmap bmp = null;
                     try
                     {
+                        if (photo.position != i)
+                        {
+                            return null;
+                        }
+                        bmp = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(photo.path, pixel), photo.thumbnail.getWidth(), photo.thumbnail.getHeight());
+                    }
+                    catch (Exception exc)
+                    {
+                        exc.printStackTrace();
+                    }
+                    return bmp;
+                }
 
+                @Override
+                protected void onPostExecute(Bitmap bmp)
+                {
+                    if(photo.position == i)
+                    {
+                        photo.thumbnail.setImageBitmap(bmp);
                     }
                 }
->>>>>>> 88666b0d6ed08a3e656ef8ee4390b081b49bb558
-            }
+            }.execute(photo);
             return convertView;
         }
     }
+
+    public ArrayList<String> indexURIs()
+    {
+        String dateAdded = MediaStore.Images.Media.DATE_ADDED;
+        String[] detailsToPull = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, detailsToPull, null, null, dateAdded);
+
+        ArrayList<String> pathsToReturn = new ArrayList<>();
+        ArrayList<String> paths = new ArrayList<>();
+
+        int imageCount = cursor.getCount();
+
+        for(int i = 0; i < imageCount; i++)
+        {
+            cursor.moveToPosition(i);
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            pathsToReturn.add(cursor.getString(index));
+        }
+        cursor.close();
+        return pathsToReturn;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Message", "Load");
+
+        if (Build.VERSION.SDK_INT >= 23)
+        {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_TO_READ_EXTERNAL_STORAGE);
+            }
+        }
+
+        Log.i("Message", "Permission");
+
+        this.listOfURIs = indexURIs();
+
+        imageAdapter = new ImageAdapter();
+        gridView = findViewById(R.id.photoGrid);
+        gridView.setAdapter(imageAdapter);
     }
 }
